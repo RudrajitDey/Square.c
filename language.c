425b352cc71aa5b042b600145946678f1e601e0a
RudrajitDey <147085819+RudrajitDey@users.noreply.github.com>
2023-10-08 22:33:45 +0530

    Add files via upload

---
+++ language.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+
+{
+    int a,b;
+    printf("Enter any number for square : ");
+    scanf("%d",&a);
+    b=a*a;
+    printf("The square is : %d",b);
+
+    return 0;
+}
